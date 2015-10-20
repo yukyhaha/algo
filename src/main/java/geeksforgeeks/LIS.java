0294a9ca32651c6ecf08afa81178e6de587e1959
Yun Zhang <yunzhang@Yuns-MBP.home>
2015-10-19 21:56:18 -0400

    unfinished LIS2

--- src/main/java/geeksforgeeks/LIS.java
+++ src/main/java/geeksforgeeks/LIS.java
@@ -1,5 +1,18 @@
 package main.java.geeksforgeeks;
 
 public class LIS {
-
+	public LIS(){}
+	
+	public int doLISRecursive(int[] arr){
+		
+	}
+	
+	public int doLISRecursiveHelper(int[] arr, int n){
+		int maxLen = Integer.MIN_VALUE;
+		for(int i = 0; i < n; i ++){
+			maxLen = Math.max(doLISRecursiveHelper(arr, i), maxLen);
+		}
+	}
+	
+	
 }
