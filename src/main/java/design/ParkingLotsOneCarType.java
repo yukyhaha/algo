b3a8037e8426da87cad805d026a28926ed14bb2b
Yun Zhang <yunzhang@Yuns-MBP.home>
2015-11-30 22:17:44 -0500

    park design

--- src/main/java/design/ParkingLotsOneCarType.java
+++ src/main/java/design/ParkingLotsOneCarType.java
@@ -1,17 +1,52 @@
 package main.java.design;
 
-class ParkingSpace{
+class Space{
 	private int id;
 	private int distanceToEntry;
-	private boolean available;
+	private boolean isAvailible;
 	
-	public ParkingSpace(){}
+	public Space(int id, int distanceToEntry){
+		this.id = id;
+		this.distanceToEntry = distanceToEntry;
+		this.isAvailible = true;
+	}
 	
 	/*getter/setter*/
 	
 }
 
 public class ParkingLotsOneCarType {
-	private static final int totalSpace = 100;
+	private static final int totalSpace = 5;	
+	private Space[] spaces = new Space[totalSpace];
+	private int availibleSpace;
+	public void createSpaces(){
+		for(int i = 0; i < totalSpace; i ++){
+			spaces[i] = new Space(i, i);
+		}
+		availibleSpace = totalSpace;
+	}
+	
+	public void heapify(Space[] spaces, int size, int index){
+		
+	}
+	
+	public void insertToHeap(Space[] spaces, int i){
+		
+	}
+	
+	public void parkCar(){
+		Space s = spaces[0];
+		spaces[0] = spaces[availibleSpace - 1];
+		availibleSpace --;
+		heapify(spaces, availibleSpace, 0);
+		s.setIsAvailible(false);
+	}
+	
+	public void unParkCar(Space s){
+		spaces[availibleSpace] = s;
+		insertToHeap(spaces, availibleSpace);
+		s.setIsAvailible(true);
+	}
+	
 	
 }
