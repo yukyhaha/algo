14753e7b6d8747759b39f2416212392995e1ad7c
Yun Zhang <yunzhang@Yuns-MBP.home>
2015-12-15 21:57:23 -0500

    1

---
+++ src/main/java/algo/MatixIsland.java
@@ -0,0 +1,51 @@
+package main.java.algo;
+
+public class MatixIsland {
+
+	public MatixIsland() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public int islandsNum(int[][] matrix){
+		int m = matrix.length;
+		int n = matrix[0].length;
+		boolean mark[][] = new boolean[m][n];
+		int count = 0;
+		for(int i = 0; i < m; i ++){
+			for(int j = 0; j < m; j ++){
+				if(matrix[i][j] == 1 && !mark[i][j]){
+					count ++;
+					visitIsland(matrix, mark, i, j);
+				}
+			}
+		}
+		return count;
+	}
+	
+	public void visitIsland(int[][] matrix, boolean[][] mark, int i, int j){
+		mark[i][j] = true;
+		if(i > 0 && matrix[i - 1][j] == 1 && !mark[i - 1][j]){
+			visitIsland(matrix, mark, i - 1, j);
+		}
+		
+		if(i + 1 < matrix.length && matrix[i + 1][j] == 1 && !mark[i + 1][j]){
+			visitIsland(matrix, mark, i + 1, j);
+		}
+		
+		if(j > 0 && matrix[i][j - 1] == 1 && !mark[i][j - 1]){
+			visitIsland(matrix, mark, i, j - 1);
+		}
+		
+		if(j + 1< matrix[0].length && matrix[i][j + 1] == 1 && !mark[i][j + 1]){
+			visitIsland(matrix, mark, i, j + 1);
+		}
+	}
+	
+	
+	public static void main(String args[]){
+		MatixIsland t = new MatixIsland();
+		int[][] matrix = {{0, 1, 0},{0, 1, 0},{0, 1, 0}};
+		System.out.println(t.islandsNum(matrix));
+	}
+
+}
--- src/main/java/algo/StringPermutation.java
+++ src/main/java/algo/StringPermutation.java
@@ -11,12 +11,24 @@ public class StringPermutation {
 	
 	public List<String> getStringPermutation(String s){
 		List<String> results = new ArrayList<String>();
-		results.add(s.substring(0, 1));
-		helper(s, results);
+		helper("", s, results);
+		return results;
+	}
+	
+	public void helper(String sub, String s, List<String> results){
+		if(s.length() == 0){
+			results.add(sub);
+			System.out.println(sub);
+			return;
+		}
+		for(int i = 0; i < s.length(); i ++){
+			helper(sub + s.charAt(i), s.substring(0, i) + s.substring(i + 1), results);			
+		}
 	}
 	
 
 	public static void main(String args[]){
-		
+		StringPermutation t = new StringPermutation();
+		t.getStringPermutation("abc");
 	}
 }
